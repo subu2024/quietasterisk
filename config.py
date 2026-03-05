83a196e275b882b00699df166520265659dc8296
Subu Sangameswar <subu.sangam@duggl.com>
2026-03-04 20:46:30 -0500

    fixed cname error in yaml file

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ from pathlib import Path
 # ==========================================================
 BLOG_TITLE = "quiet asterisk"
 TAG_LINE = "On chance, meaning, and the quiet details"
-COPYRIGHT = "© 2026 pathway llc. All rights reserved. v1.0.01"
+COPYRIGHT = "© 2026 pathway llc. All rights reserved. v1.0.02"
 CONTACT_EMAIL = "hello@quietasterisk.com"
 
 # ==========================================================
